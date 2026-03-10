72c88238f3592e44ea71c6f5cf4d459f74b33a37
NelsonD <2004nelsondaniel@gmail.com>
2026-03-09 18:17:04 -0600

    Agregar DTo, IService y Service de TempPacking para consumo de API

---
+++ Data/Sew/DTO/TempPackingDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace ReportPackaging.Data.Sew.DTO
+{
+    public class TempPackingDTO
+    {
+        public int Id { get; set; }
+        public string Buyer { get; set; } = string.Empty;
+        public string Style { get; set; } = string.Empty;
+        public string Po { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Orden { get; set; } = string.Empty;
+        public int Xs { get; set; }
+        public int S { get; set; }
+        public int M { get; set; }
+        public int L { get; set; }
+        public int Xl { get; set; }
+        public int Xl2 { get; set; }
+        public int Xl3 { get; set; }
+        public int TodayTotal { get; set; }
+    }
+}
---
+++ Data/Sew/IService/ITempPackingService.cs
@@ -0,0 +1,11 @@
+﻿using ReportPackaging.Data.Sew.DTO;
+
+namespace ReportPackaging.Data.Sew.IService
+{
+    public interface ITempPackingService
+    {
+        //Task<IEnumerable<TempPackingDTO>> GetAllAsync();
+        Task<bool> InsertAsync(TempPackingDTO item);
+        //Task<bool> UpdateAsync(TempPackingDTO item);
+    }
+}
